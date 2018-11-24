0214b5757e511ca5c746c2ca8798ec15d778097b
ge-hall <Andrew.Ge-Hall@healthcarelogic.com.au>
2018-11-24 17:51:37 +1000

    Front end hot relaoding

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 
 import App from './App';
 import './index.css';
@@ -8,9 +8,31 @@ import './index.css';
 import configureStore from "./store/configureStore";
 const store = configureStore();
 
-ReactDOM.render(
+const rootEl = document.getElementById('root');
+
+// turn render into function
+let render = () => {
+  // Dynamically import our main App component, and render it
+  // eslint-disable-next-line
+  const App = require("./App").default; // eslint-disable-line
+
+  ReactDOM.render(
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>,
-    document.getElementById('root')
-);
+    rootEl
+  );
+}
+
+if(process.env.NODE_ENV !== "production") {
+  if(module.hot) {
+      // Support hot reloading of components.
+      // Whenever the App component file or one of its dependencies
+      // is changed, re-import the updated component and re-render it
+      module.hot.accept("./App", () => {
+          setTimeout(render);
+      });
+  }
+}
+
+render();
